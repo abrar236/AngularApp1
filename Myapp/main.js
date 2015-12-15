e159e558101cb69d8bcba66eb82b2639bf60a5cb
ABRAR KHAN <ABRAR KHAN>
2015-12-14 18:20:17 -0800

    second commit

--- Myapp/main.js
+++ Myapp/main.js
@@ -1,10 +1,19 @@
 var app=angular.module('minmax',[]);
 
-app.controller('MinMaxCtrl',function($scope){
+app.controller('MinMaxCtrl',function($scope,$http){
 $scope.formModel={};
 
 $scope.onSubmit=function(){
 
 	console.log("Im submitted");
+	console.log($scope.formModel);
+
+	$http.post('https://minmax-server.herokuapp.com/register/',$scope.formModel).
+	success(function(data){
+		console.log('happy');
+	}).error(function(data){
+
+		console.log('not happy');
+	})
 }
 });
